9dc6aeca5aadde6bd1a2bf88d03e062041720002
unknown <yeasalehofficial2002@gmail.com>
2021-11-12 22:15:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6afp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run() {
+    try {
+      await client.connect();
+      console.log('Database connected')
+
+    }
+
+
+finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Motoz Lover!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Motoz at http://localhost:${port}`)
+  })
+  
