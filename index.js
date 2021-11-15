3728b51358c80dd4547d88c2bd1828a96536c719
unknown <yeasalehofficial2002@gmail.com>
2021-11-16 00:11:24 +0600

    dash

--- index.js
+++ index.js
@@ -21,13 +21,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
       try {
         await client.connect();
         console.log('Database connected')
-
-      
+     
         
          //  database and collection
          const database = client.db('bikes-shop-database');
          const productsCollection = database.collection('products');
-
+         const ordersCollection = database.collection('oders');
 
          
       
@@ -42,14 +41,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         //GET single product
         app.get('/products/:id', async (req, res) => {
           const id = req.params.id;
-          console.log('getting single products', id);
           const  query = { _id: ObjectID(id) };
           const product = await productsCollection.findOne(query);
           res.json(product);
       })
 
 
+   
 
+      //----------- get my Order 
+      app.get('/orders', async ( req, res) => {
+          
+    
+        const email = req.query?.email;
+        const query = {email : email}
+         console.log(query)
+
+
+        const cursor = ordersCollection.find(query);
+         const orders = await cursor.toArray();
+         res.json(orders);
+       })
+
+
+
+
+      //-------- post data for orders-------
+      app.post('/oders', async (req , res) => {
+        const order = req.body;
+        const result = await ordersCollection.insertOne(order);
+        res.json(result)
+      });
 
 
 
